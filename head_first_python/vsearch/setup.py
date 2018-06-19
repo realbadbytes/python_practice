6bf61fd627f216beda376f0f8ba7d69f72155ba6
realbadbytes1 <badbytesio@gmail.com>
2018-06-18 22:58:07 -0400

    module creation, installation, and flask API using new module

---
+++ head_first_python/vsearch/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name = 'vsearch',
+    version = '1.0',
+    description = 'The Head First Python Search Tools',
+    author = 'realbadbytes',
+    author_email = 'test@noob.com',
+    url = 'test.com',
+    # The list of py files to include in the package
+    py_modules = ['vsearch'])
