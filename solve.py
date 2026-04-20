50deaa0a5c2755911b38d638e51b6d09a5341d09
badbytes <badbytesio@gmail.com>
2026-04-20 17:03:42 -0400

    prog chal

---
+++ solve.py
@@ -0,0 +1,54 @@
+'''
+Return number of cells that fall within N steps of any positive values in a 2d array
+Step distance is defined by Manhattan Distance: abs(x1-x2) + abs(y1-y2)
+Array does not wrap unless height and/or width is less than 3
+
+Expansion question possibilities
+"what if it does wrap for all sizes"
+
+Optimizations
+short circuit if candidate cell falls within a certain range based on WxH
+'''
+
+import numpy as np
+
+def visualize_hood(arr, c):
+  for h in c:
+    arr[h] = 1
+  print(arr)
+
+def manhattan_distance(c, n):
+  cells = []
+  c_x, c_y = c[0], c[1]
+  for x in range(c_x - n, c_x + n+1):
+    for y in range(c_y - n, c_y + n+1):
+      man_dist = abs(x - c_x) + abs(y - c_y)
+      if man_dist <= n:
+        print(f'[+] {man_dist}')
+        cells.append( [x, y] )
+  print(f'Candidates size: {len(cells)}')
+  return cells
+
+def num_cells(arr, steps: int):
+  print(f'steps: {steps}')
+  # find all positive values in the array
+  candidates = np.argwhere(arr > 0)
+
+  # for each candidate, find valid cells within steps distance
+  candidates = {tuple(candidates): [] for candidates in candidates}
+  print(f'candidates: {candidates}')
+
+  # Find the neighborhood. calc manhattan distance from cell
+  for c in candidates:
+    candidates[c] = manhattan_distance(c, steps)
+  print(candidates)
+
+  visualize_hood(arr, candidates)
+
+  # mask invalid results (wrapping) based on NxM condition
+
+if __name__ == '__main__':
+  test = np.zeros((11,11))
+  test[6, 6] = 5
+  print(test)
+  num_cells(test, 3)
