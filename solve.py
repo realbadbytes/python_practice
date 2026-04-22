da621b5621c349458cebefbb6e5ce3c52eb66c88
badbytes <badbytesio@gmail.com>
2026-04-22 08:53:42 -0400

    oddly shaped array example, two hoods with one wraparound example, fix logic with visualization (y,x)

--- solve.py
+++ solve.py
@@ -34,7 +34,8 @@ def visualize_hood(grid_x, grid_y, candidates):
   xmin, xmax = 0, grid_x
   ymin, ymax = 0, grid_y
 
-  grid = np.zeros((grid_x, grid_y))
+  # matrix syntax, y, x
+  grid = np.zeros((grid_y, grid_x))
 
   for xi, yi in coords:
     grid[yi - ymin, xi - xmin] = 1  # note: [y, x]
@@ -93,10 +94,16 @@ def num_cells(arr, steps: int):
     candidates[c] = manhattan_distance(c, steps)
   print(candidates)
 
-  visualize_hood(11, 11, candidates)
+  # matrix notation for numpy, arr.shape is tuple (y,x)
+  visualize_hood(arr.shape[1], arr.shape[0], candidates)
 
   cell_count = 0
-  coords = [sublist for lists in candidates.values() for sublist in lists]
+  #coords = [sublist for lists in candidates.values() for sublist in lists]
+  coords = []
+  for cells in candidates.values():
+    for cell in cells:
+      coords.append(cell)
+  print(f'All coords before unique filtering for overlap: {coords}')
   unique = [list(t) for t in {tuple(x) for x in coords}]
   return len(unique)
 
@@ -108,47 +115,44 @@ if __name__ == '__main__':
   # return 25
   ex_1 = np.zeros((11,11))
   ex_1[5,5] = 9
-  print(f'\033[92m Neighbors: {num_cells(ex_1, 3)} \033[0m')
+  print(f'\033[92m Neighbors: {num_cells(ex_1, 3)} == 25 \033[0m')
 
   # one positive cell near edge
   # return 21
   ex_2 = np.zeros((11,11))
   ex_2[1,5] = 9
-  print(f'\033[92m Neighbors: {num_cells(ex_2, 3)} \033[0m')
+  print(f'\033[92m Neighbors: {num_cells(ex_2, 3)} == 21 \033[0m')
 
   # two positive values with disjoint neighborhoods
   # return 26
   ex_3 = np.zeros((11,11))
   ex_3[3,3] = 9
   ex_3[7,7] = 9
-  print(f'\033[92m Neighbors: {num_cells(ex_3, 2)} \033[0m')
+  print(f'\033[92m Neighbors: {num_cells(ex_3, 2)} == 26 \033[0m')
 
   # two positive values with overlapping neighborhoods
   # test counting of candidate cells only once per grid
   # return 22
+  # 23 should be the correct answer?
+  # the overlap is 3 cells
   ex_4 = np.zeros((11,11))
   ex_4[3,3] = 9
   ex_4[5,5] = 9
-  print(f'\033[92m Neighbors: {num_cells(ex_4, 2)} \033[0m')
+  print(f'\033[92m Neighbors: {num_cells(ex_4, 2)} == 22 \033[0m')
 
+  # two positive values with one running off
+  # should have 1 cell clipped
+  # return 22
+  ex_5 = np.zeros((11,11))
+  ex_5[3,3] = 9
+  ex_5[1,5] = 9
+  print(f'\033[92m Neighbors: {num_cells(ex_5, 2)} == 22 \033[0m')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  # oddly shaped array, long x axis
+  ex_6 = np.zeros((15, 30))
+  ex_6[3,3] = 9
+  ex_6[7,7] = 9
+  print(f'\033[92m Neighbors: {num_cells(ex_6, 2)} == 26 \033[0m')
 
 
 
